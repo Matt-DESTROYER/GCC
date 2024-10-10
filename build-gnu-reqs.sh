d7e9cafd0884e1a9ef23b6d9b1b95cb33a886190
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-10 14:19:23 +1100

    Update build-gnu-reqs.sh

--- build-gnu-reqs.sh
+++ build-gnu-reqs.sh
@@ -4,42 +4,41 @@
 curl -Lo gmp-6.3.0.tar.xz https://gmplib.org/download/gmp/gmp-6.3.0.tar.xz
 tar xf gmp-6.3.0.tar.xz
 rm gmp-6.3.0.tar.xz
-mkdir gmp
-cd gmp
-../gmp-6.3.0/configure
+cd ./gmp-6.3.0
+./configure
 make
+make install
 cd ../
-rm -rf gmp-6.3.0
 
 # MPFR 4.2.1
 curl -Lo mpfr-4.2.1.tar.xz https://www.mpfr.org/mpfr-current/mpfr-4.2.1.tar.xz
 tar xf mpfr-4.2.1.tar.xz
 rm mpfr-4.2.1.tar.xz
 mkdir mpfr
-cd mpfr
-../mpfr-4.2.1/configure
+cd ./mpfr-4.2.1
+./configure --with-gmp=../gmp-6.3.0
 make
+make install
 cd ../
-rm -rf mpfr-4.2.1
 
 # MPC 1.3.1
 curl -Lo mpc-1.3.1.tar.gz https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz
 tar xf mpc-1.3.1.tar.gz
 rm mpc-1.3.1.tar.gz
 mkdir mpc
-cd mpc
-../mpc-1.3.1/configure
+cd ./mpc-1.3.1
+./configure --with-gmp=../gmp-6.3.0 --with-mpfr=../mpfr-4.2.1
 make
+make install
 cd ../
-rm -rf mpc-1.3.1
 
 # ISL 0.27
 curl -Lo isl-0.27.tar.xz https://libisl.sourceforge.io/isl-0.27.tar.xz
 tar xf isl-0.27.tar.xz
 rm isl-0.27.tar.xz
 mkdir isl
-cd isl
-../isl-0.27/configure
+cd ./isl-0.27
+./configure --with-gmp=../gmp-6.3.0 --with-mpfr=../mpfr-4.2.1 --with-mpc=../mpc-1.3.1
 make
+make install
 cd ../
-rm -rf isl-0.27
