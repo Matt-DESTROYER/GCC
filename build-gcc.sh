5780fceeb905b6eeec6c87f9857899d912ebbd67
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-11 10:50:03 +1100

    Create build-gcc.sh

---
+++ build-gcc.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+cd ./gcc
+./configure --enable-languages=all --with-gmp=../gmp-6.3.0 --with-mpfr=../mpfr-4.2.1 --with-mpc=../mpc-1.3.1 --with-isl=../isl-0.27
+cd build
+make
+cd ../../
+mv ./gcc/build ./
+rm -rf ./gcc
