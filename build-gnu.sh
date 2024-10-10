2c0766c2ff492ed8cc333ac78bca0ebd5148b630
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-11 10:43:40 +1100

    Update build-gnu.sh

--- build-gnu.sh
+++ build-gnu.sh
@@ -5,6 +5,7 @@ curl -Lo m4-1.4.tar.gz https://ftp.gnu.org/gnu/m4/m4-1.4.tar.gz
 tar xf m4-1.4.tar.gz
 rm m4-1.4.tar.gz
 cd ./m4-1.4
+bash ../Windows-clean-files.sh
 ./configure
 make
 make install
@@ -15,6 +16,7 @@ curl -Lo gmp-6.3.0.tar.xz https://ftp.gnu.org/gnu/gmp/gmp-6.3.0.tar.xz
 tar xf gmp-6.3.0.tar.xz
 rm gmp-6.3.0.tar.xz
 cd ./gmp-6.3.0
+bash ../Windows-clean-files.sh
 ./configure
 make
 make install
@@ -25,6 +27,7 @@ curl -Lo mpfr-4.2.1.tar.xz https://ftp.gnu.org/gnu/mpfr/mpfr-4.2.1.tar.xz
 tar xf mpfr-4.2.1.tar.xz
 rm mpfr-4.2.1.tar.xz
 cd ./mpfr-4.2.1
+bash ../Windows-clean-files.sh
 ./configure --with-gmp=../gmp-6.3.0
 make
 make install
@@ -35,6 +38,7 @@ curl -Lo mpc-1.3.1.tar.gz https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz
 tar xf mpc-1.3.1.tar.gz
 rm mpc-1.3.1.tar.gz
 cd ./mpc-1.3.1
+bash ../Windows-clean-files.sh
 ./configure --with-gmp=../gmp-6.3.0 --with-mpfr=../mpfr-4.2.1
 make
 make install
@@ -45,6 +49,7 @@ curl -Lo isl-0.27.tar.xz https://libisl.sourceforge.io/isl-0.27.tar.xz
 tar xf isl-0.27.tar.xz
 rm isl-0.27.tar.xz
 cd ./isl-0.27
+bash ../Windows-clean-files.sh
 ./configure --with-gmp=../gmp-6.3.0 --with-mpfr=../mpfr-4.2.1 --with-mpc=../mpc-1.3.1
 make
 make install
