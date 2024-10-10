e8368417e510644d35a043f04226389637ef6ebc
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-10 14:23:06 +1100

    Update build-gnu-reqs.sh

--- build-gnu-reqs.sh
+++ build-gnu-reqs.sh
@@ -14,7 +14,6 @@ cd ../
 curl -Lo mpfr-4.2.1.tar.xz https://www.mpfr.org/mpfr-current/mpfr-4.2.1.tar.xz
 tar xf mpfr-4.2.1.tar.xz
 rm mpfr-4.2.1.tar.xz
-mkdir mpfr
 cd ./mpfr-4.2.1
 ./configure --with-gmp=../gmp-6.3.0
 make
@@ -25,7 +24,6 @@ cd ../
 curl -Lo mpc-1.3.1.tar.gz https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz
 tar xf mpc-1.3.1.tar.gz
 rm mpc-1.3.1.tar.gz
-mkdir mpc
 cd ./mpc-1.3.1
 ./configure --with-gmp=../gmp-6.3.0 --with-mpfr=../mpfr-4.2.1
 make
@@ -36,7 +34,6 @@ cd ../
 curl -Lo isl-0.27.tar.xz https://libisl.sourceforge.io/isl-0.27.tar.xz
 tar xf isl-0.27.tar.xz
 rm isl-0.27.tar.xz
-mkdir isl
 cd ./isl-0.27
 ./configure --with-gmp=../gmp-6.3.0 --with-mpfr=../mpfr-4.2.1 --with-mpc=../mpc-1.3.1
 make
