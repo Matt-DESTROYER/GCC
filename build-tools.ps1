bfd587e83496a2c98fe9af655b5408a6d4a14b30
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-11 12:48:11 +1100

    Include `make`

--- build-tools.ps1
+++ build-tools.ps1
@@ -1,4 +1,4 @@
 choco install cygwin
 choco install cyg-get
 
-cyg-get gcc-core gcc-g++ libgcc1 libgccpp1 gdb gzip m4 libgmp-devel libmpfr-devel libmpc-devel libisl-devel w32api-headers w32api-runtime binutils perl
+cyg-get gcc-core gcc-g++ libgcc1 libgccpp1 gdb gzip m4 libgmp-devel libmpfr-devel libmpc-devel libisl-devel w32api-headers w32api-runtime binutils perl bash bash-devel make
