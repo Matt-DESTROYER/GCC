faa93a76f0a5e6d5f61a45c9968d325ecff57c80
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-11 12:45:32 +1100

    Bare minimum

--- build-tools.ps1
+++ build-tools.ps1
@@ -1,10 +1,4 @@
-wsl --install
-
-choco upgrade all
-
-choco install python
-
-choco install cyg-get
 choco install cygwin
+choco install cyg-get
 
 cyg-get gcc-core gcc-g++ libgcc1 libgccpp1 gdb gzip m4 libgmp-devel libmpfr-devel libmpc-devel libisl-devel w32api-headers w32api-runtime binutils perl
