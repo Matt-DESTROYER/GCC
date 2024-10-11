da6a29b36bffe1769e5ac3c3d3c47c3d565717ef
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-11 11:42:20 +1100

    Prefer scripts over using `bash` in action files

---
+++ build-tools.sh
@@ -0,0 +1,12 @@
+choco upgrade all
+
+choco install gzip
+choco install bzip2
+choco install sed
+
+choco install python
+choco install strawberryperl
+
+choco install gnuwin
+choco install make
+choco install cygwin
